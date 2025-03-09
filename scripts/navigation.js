bc8702b8d2aa7ee0b95d96047f5ccdc7f17f5d7f
profepabloheredia <profepabloheredia@gmail.com>
2025-03-09 02:02:36 -0300

    changes

---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+
+
+const mainnav = document.querySelector('.menu');
+const hambutton = document.querySelector('#menuButton');
+
+hambutton.addEventListener('click', () =>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
