0042f0c774d90d703c5716d7258d8514c97a9d67
profepabloheredia <profepabloheredia@gmail.com>
2025-05-19 22:19:48 -0300

    .

--- modules/scripts/modules.js
+++ modules/scripts/modules.js
@@ -7,16 +7,16 @@ document.querySelector("#enrollStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
   byuiCourse.changeEnrollment(sectionNum);
   renderSections(byuiCourse.sections);
-  alert("1");
+  alert("1 Student enrolled");alert("1 Student enrolled to section " + sectionNum);
   
 });
 document.querySelector("#dropStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
   byuiCourse.changeEnrollment(sectionNum, false);
   renderSections(byuiCourse.sections);
-  alert("2");
+  alert("1 Student droped from section " + sectionNum);
 });
-alert("3");
+
 setTitle(byuiCourse);
 setSectionSelection(byuiCourse.sections);
 renderSections(byuiCourse.sections);
