bf69dceca8a7c6c7f3bd9b5abec3bd54d8f931c7
profepabloheredia <profepabloheredia@gmail.com>
2025-05-20 23:48:38 -0300

    Api created

---
+++ api/scripts/api.js
@@ -0,0 +1,33 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const myKey="7806e6378615e3ce5201964e721cdac1";
+const lat="49.75";
+const long="6.64";
+
+const url = "http://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7806e6378615e3ce5201964e721cdac1&units=metric";
+async function apiFetch() {  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
