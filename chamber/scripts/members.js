6b82a80dd5bf8cafefef6aed8d9a96ec30f951e5
profepabloheredia <profepabloheredia@gmail.com>
2025-05-31 23:35:08 -0300

    Fix some errors

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -56,3 +56,17 @@ const displayMembers = (members) => {
 getMemberData();
 
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#cards");
+
+gridbutton.addEventListener('click', () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener('click', () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+});
+
--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -23,17 +23,3 @@ if (hamburgerBtn && primaryNav) {
         });
     });
 }
-
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("#cards");
-
-gridbutton.addEventListener('click', () => {
-    display.classList.add("grid");
-    display.classList.remove("list");
-});
-
-listbutton.addEventListener('click', () => {
-    display.classList.add("list");
-    display.classList.remove("grid");
-});
